855eb9ed397e1e66a2787db7240efe5ba3ac1a99
Martin Golub <martin.golub@fer.unizg.hr>
2025-12-04 10:17:37 +0100

    Interaction system

--- scenes/entities/bradley/first_person_camera.gd
+++ scenes/entities/bradley/first_person_camera.gd
@@ -10,13 +10,9 @@ class_name FirstPersonCamera
 			Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		captured = value
 		
-#var collided: bool = false
-#var collider: Node3D
-#@onready var RAY := $RayCast3D
-#signal on_hover(target: Node3D)
-#signal off_hover(target: Node3D)
-#signal on_press(target: Node3D)
-#signal on_release(target: Node3D)
+@onready var RAY := $RayCast3D
+var collided: bool = false
+var collider: PhysicsBody3D = null
 
 func _input(event):
 	if event is InputEventMouseMotion && captured:
@@ -25,17 +21,49 @@ func _input(event):
 		rotation_degrees.y -= event.relative.x * SENSITIVITY
 	elif (event.is_action_pressed("alt_look")):
 		captured = !captured
-	#elif (event.is_action("interact") and collided):
-		#if (event.is_action_pressed("interact")):
-			#on_press.emit(collider)
-		#elif (event.is_action_released("interact")):
-			#on_release.emit(collider)
+	elif (event.is_action("interact") and collided):
+		if (event.is_action_pressed("interact") and collider.has_method("press")):
+			collider.press()
+		elif (event.is_action_released("interact") and collider.has_method("release")):
+			collider.release()
 		
-#func _physics_process(delta: float) -> void:
-	#if (RAY.is_colliding() != collided):
-		#if (RAY.is_colliding()):
-			#collider = RAY.get_collider()
-			#on_hover.emit(collider)
-		#else:
-			#off_hover.emit(collider)
-		#collided = RAY.is_colliding()
+func _physics_process(_delta: float) -> void:
+	if (captured):
+		if (RAY.is_colliding() != collided):
+			if (RAY.is_colliding()):
+				collider = RAY.get_collider()
+				if (collider.has_method("enter")):
+					collider.enter()
+			else:
+				if (collider.has_method("exit")):
+					collider.exit()
+				collider = null
+			collided = RAY.is_colliding()
+			
+	else:
+		var ray_cast = cursor_ray_cast()
+		if (!ray_cast.is_empty() != collided):
+			if (ray_cast.is_empty()):
+				if (collider.has_method("exit")):
+					collider.exit()
+				collider = null
+			else:
+				collider = ray_cast.collider
+				if (collider.has_method("enter")):
+					collider.enter()
+			collided = !ray_cast.is_empty()
+
+# https://www.youtube.com/watch?v=5Uc9yzj4YLY
+func cursor_ray_cast() -> Dictionary:
+	var mouse = get_viewport().get_mouse_position()
+	var space = get_world_3d().direct_space_state
+	var start = get_viewport().get_camera_3d().project_ray_origin(mouse)
+	var end = get_viewport().get_camera_3d().project_position(mouse, abs(RAY.target_position.y))
+	
+	var params = PhysicsRayQueryParameters3D.new()
+	params.from = start
+	params.to = end
+	var result = space.intersect_ray(params)
+	return result
+	
+	
---
+++ scripts/area_3d_test.gd
@@ -0,0 +1,13 @@
+extends StaticBody3D
+
+func enter():
+	print("enter")
+	
+func exit():
+	print("exit")
+	
+func press():
+	print("press")
+	
+func release():
+	print("release")
